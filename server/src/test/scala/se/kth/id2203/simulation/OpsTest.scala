c8df68929c6a1ea5fc80a453d57ed913aae75817
Marta Bertran <m.bertran@be12.ch>
2020-02-12 17:34:21 +0100

    Initial tests of infrastructure

--- server/src/test/scala/se/kth/id2203/simulation/OpsTest.scala
+++ server/src/test/scala/se/kth/id2203/simulation/OpsTest.scala
@@ -23,17 +23,18 @@
  */
 package se.kth.id2203.simulation
 
+import java.net.{InetAddress, UnknownHostException}
+
 import org.scalatest._
-import se.kth.id2203.ParentComponent;
-import se.kth.id2203.networking._;
+import se.kth.id2203.ParentComponent
+import se.kth.id2203.networking._
 import se.sics.kompics.network.Address
-import java.net.{ InetAddress, UnknownHostException };
-import se.sics.kompics.sl._;
-import se.sics.kompics.sl.simulator._;
-import se.sics.kompics.simulator.{ SimulationScenario => JSimulationScenario }
+import se.sics.kompics.simulator.result.SimulationResultSingleton
 import se.sics.kompics.simulator.run.LauncherComp
-import se.sics.kompics.simulator.result.SimulationResultSingleton;
-import se.sics.kompics.simulator.network.impl.NetworkModels
+import se.sics.kompics.simulator.{SimulationScenario => JSimulationScenario}
+import se.sics.kompics.sl._
+import se.sics.kompics.sl.simulator._
+
 import scala.concurrent.duration._
 
 class OpsTest extends FlatSpec with Matchers {
@@ -58,7 +59,7 @@ class OpsTest extends FlatSpec with Matchers {
   //  }
 
   "Simple Operations" should "not be implemented" in { // well of course eventually they should be implemented^^
-    val seed = 123l;
+    /*val seed = 123l;
     JSimulationScenario.setSeed(seed);
     val simpleBootScenario = SimpleScenario.scenario(3);
     val res = SimulationResultSingleton.getInstance();
@@ -129,6 +130,112 @@ object SimpleScenario {
       0.seconds afterTermination startCluster andThen
       10.seconds afterTermination startClients andThen
       100.seconds afterTermination Terminate
+  }*/
+
+    val seed = 123l
+    JSimulationScenario.setSeed(seed)
+    // If we have 3 servers, we get one group with only one member which is not supported.
+    val simpleBootScenario = SimpleScenario.scenario(20)
+    val res = SimulationResultSingleton.getInstance()
+
+    SimulationResult += ("operations" -> "NOP")
+    SimulationResult += ("nMessages" -> nMessages)
+
+    simpleBootScenario.simulate(classOf[LauncherComp])
+
+    for (i <- 0 to nMessages) {
+      SimulationResult.get[String](s"test$i") should be(Some("None"))
+    }
   }
+
+
+  "Write then Read" should "read the writen value" in { // well of course eventually they should be implemented^^
+    val seed = 123l
+    JSimulationScenario.setSeed(seed)
+    val simpleBootScenario = SimpleScenario.scenario(20)
+    val res = SimulationResultSingleton.getInstance()
+
+    SimulationResult += ("operations" -> "ReadWrite")
+    SimulationResult += ("nMessages" -> nMessages)
+
+    simpleBootScenario.simulate(classOf[LauncherComp])
+
+    for (i <- 0 to nMessages) {
+      SimulationResult.get[String](s"test$i") should be(Some((s"$i")))
+    }
+  }
+
+  /*"Compare and swap" should "swap the values if they are correct" in { // well of course eventually they should be implemented^^
+    val seed = 123l
+    JSimulationScenario.setSeed(seed)
+    val simpleBootScenario = SimpleScenario.scenario(20)
+    val res = SimulationResultSingleton.getInstance()
+
+    SimulationResult += ("operations" -> "CAS")
+    SimulationResult += ("nMessages" -> nMessages)
+
+    simpleBootScenario.simulate(classOf[LauncherComp])
+
+    for (i <- 0 to nMessages) {
+      SimulationResult.get[String](s"test$i") should be(Some((s"$i")))
+    }
+  }*/
+
+}
+
+object SimpleScenario {
+
+  import Distributions._
+  // needed for the distributions, but needs to be initialised after setting the seed
+  implicit val random = JSimulationScenario.getRandom()
+
+  private def intToServerAddress(i: Int): Address = {
+    try {
+      NetAddress(InetAddress.getByName("192.193.0." + i), 45678)
+    } catch {
+      case ex: UnknownHostException => throw new RuntimeException(ex)
+    }
+  }
+  private def intToClientAddress(i: Int): Address = {
+    try {
+      NetAddress(InetAddress.getByName("192.193.1." + i), 45678)
+    } catch {
+      case ex: UnknownHostException => throw new RuntimeException(ex)
+    }
+  }
+
+  private def isBootstrap(self: Int): Boolean = self == 1
+
+  val startServerOp = Op { (self: Integer) =>
+
+    val selfAddr = intToServerAddress(self)
+    val conf = if (isBootstrap(self)) {
+      // don't put this at the bootstrap server, or it will act as a bootstrap client
+      Map("id2203.project.address" -> selfAddr)
+    } else {
+      Map(
+        "id2203.project.address" -> selfAddr,
+        "id2203.project.bootstrap-address" -> intToServerAddress(1))
+    }
+    StartNode(selfAddr, Init.none[ParentComponent], conf)
+  }
+
+  val startClientOp = Op { (self: Integer) =>
+    val selfAddr = intToClientAddress(self)
+    val conf = Map(
+      "id2203.project.address" -> selfAddr,
+      "id2203.project.bootstrap-address" -> intToServerAddress(1))
+    StartNode(selfAddr, Init.none[ScenarioClient], conf)
+  }
+
+  def scenario(servers: Int): JSimulationScenario = {
+    val startCluster = raise(servers, startServerOp, 1.toN).arrival(constant(1.second))
+    val startClients = raise(1, startClientOp, 1.toN).arrival(constant(1.second))
+
+    startCluster andThen
+      100.seconds afterTermination startClients andThen
+      200.seconds afterTermination Terminate
+  }
+
 
 }
--- server/src/test/scala/se/kth/id2203/simulation/ScenarioClient.scala
+++ server/src/test/scala/se/kth/id2203/simulation/ScenarioClient.scala
@@ -23,16 +23,13 @@
  */
 package se.kth.id2203.simulation;
 
-import java.util.UUID;
-import se.kth.id2203.kvstore._;
-import se.kth.id2203.networking._;
-import se.kth.id2203.overlay.RouteMsg;
+import se.kth.id2203.kvstore.{Op, OpResponse, Operation}
+import se.kth.id2203.networking._
+import se.kth.id2203.overlay.RouteMsg
+import se.sics.kompics.network.Network
 import se.sics.kompics.sl._
-import se.sics.kompics.Start;
-import se.sics.kompics.network.Network;
+import se.sics.kompics.sl.simulator.SimulationResult
 import se.sics.kompics.timer.Timer;
-import se.sics.kompics.sl.simulator.SimulationResult;
-import collection.mutable;
 
 class ScenarioClient extends ComponentDefinition {
 
@@ -42,13 +39,15 @@ class ScenarioClient extends ComponentDefinition {
   //******* Fields ******
   val self = cfg.getValue[NetAddress]("id2203.project.address");
   val server = cfg.getValue[NetAddress]("id2203.project.bootstrap-address");
-  private val pending = mutable.Map.empty[UUID, String];
+  var pending: Option[Operation] = None
+  var op_index = 0
+  var Ops = List.empty[Operation]
   //******* Handlers ******
-  ctrl uponEvent {
+  /*ctrl uponEvent {
     case _: Start => {
       val messages = SimulationResult[Int]("messages");
       for (i <- 0 to messages) {
-        val op = new Op(s"test$i");
+        val op = new Op("PUT", "2", "hello");
         val routeMsg = RouteMsg(op.key, op); // don't know which partition is responsible, so ask the bootstrap server to forward it
         trigger(NetMessage(self, server, routeMsg) -> net);
         pending += (op.id -> op.key);
@@ -59,12 +58,89 @@ class ScenarioClient extends ComponentDefinition {
   }
 
   net uponEvent {
-    case NetMessage(header, or @ OpResponse(id, status)) => {
+    case NetMessage(header, or @ OpResponse(id, status, value)) => {
       logger.debug(s"Got OpResponse: $or");
       pending.remove(id) match {
         case Some(key) => SimulationResult += (key -> status.toString());
         case None      => logger.warn("ID $id was not pending! Ignoring response.");
       }
+    }
+  }*/
+  ctrl uponEvent {
+    case _: Start => {
+      val op_type: String = SimulationResult[String]("operations")
+      val nMessages = SimulationResult[Int]("nMessages")
+
+      if (op_type == "NOP" ) {
+        for (i <- 0 to nMessages) {
+          Ops ++= List(new se.kth.id2203.kvstore.Op("GET", s"test$i", ""))
+        }
+      } else if (op_type == "ReadWrite" ) {
+        for (i <- 0 to nMessages) {
+          Ops ++= List(new Op("PUT", s"test$i", s"$i"))
+          Ops ++= List(new Op("GET", s"test$i", ""))
+        }
+      } else if (op_type == "CAS" ) {
+        for (i <- 0 to nMessages) {
+          // Shoud result in a list with the values 0..nValues
+          if (i < nMessages / 2)
+            Ops ++= List(new Op("PUT", s"test$i", s"$i"))
+          else
+            Ops ++= List(new Op("PUT", s"test$i", s"1"))
+        } // Ops = (1,2,3, ..., nMessages/2, 1, 1, ... )
+
+        /*for (i <- 0 to nMessages) {
+          if (i < nMessages / 2)
+            Ops ++= List(new CompareAndSwap(s"test$i", "1", Some("1"))) // Should not do anything
+          else
+            Ops ++= List(new CompareAndSwap(s"test$i", s"$i", Some("1"))) // Should replace the second half with correct values
+        }*/
+        for (i <- 0 to nMessages) {
+          Ops ++= List(new Op("GET",s"test$i", ""))
+        }
+      }
+
+
+      sendMessage()
+    }
+  }
+
+  def sendMessage() = {
+    val op = Ops(op_index)
+    op_index = op_index + 1
+    pending = Some(op)
+    val routeMsg = RouteMsg(op.key, op) // don't know which partition is responsible, so ask the bootstrap server to forward it
+    trigger(NetMessage(self, server, routeMsg) -> net)
+
+    logger.info("Sending {}", op)
+    //    SimulationResult += (op.key -> "Sent")
+  }
+
+  net uponEvent {
+    case NetMessage(header, opResp@OpResponse(id, status, value)) => {
+      logger.debug(s"Got OpResponse: $id")
+
+      if (pending.isDefined && pending.get.id == id) {
+        val valueString = value match {
+          case Some(v) => s"$v";
+          case None => "None";
+        }
+
+        SimulationResult += (pending.get.key -> valueString)
+
+        printf(s"\nSETTING: ${pending.get.key -> valueString}\n")
+
+        if (op_index < Ops.size) {
+          sendMessage()
+        }
+
+      } else {
+        printf(s"ID was not pending! Ignoring response message: $opResp\n")
+      }
+      // Should be here!
+      //      if (op_index < Ops.size) {
+      //        sendMessage()
+      //      }
     }
   }
 }
