84c05bc95ff6648d9352e961779dbb2637bb09be
RSvenja <svenja.raether@tum.de>
2020-02-21 14:50:34 +0100

    reconfiguration to be continued

--- server/src/main/scala/se/kth/id2203/consensus/LeaderBasedSequencePaxos.scala
+++ server/src/main/scala/se/kth/id2203/consensus/LeaderBasedSequencePaxos.scala
@@ -23,6 +23,7 @@
  */
 package se.kth.id2203.consensus
 
+import se.kth.id2203.kvstore.{Op, OperationToPropose}
 import se.kth.id2203.networking.{NetAddress, NetMessage}
 import se.sics.kompics.sl._
 import se.sics.kompics.network._
@@ -30,17 +31,17 @@ import se.sics.kompics.KompicsEvent
 
 import scala.collection.mutable
 
-  case class Prepare(nL: Long, ld: Int, na: Long) extends KompicsEvent;
+  case class Prepare(nL: (Int,Int) , ld: Int, na: (Int,Int) )extends KompicsEvent;
 
-  case class Promise(nL: Long, na: Long, suffix: List[RSM_Command], ld: Int) extends KompicsEvent;
+  case class Promise(nL: (Int,Int), na: (Int,Int), suffix: List[RSM_Command], ld: Int) extends KompicsEvent;
 
-  case class AcceptSync(nL: Long, suffix: List[RSM_Command], ld: Int) extends KompicsEvent;
+  case class AcceptSync(nL: (Int, Int), suffix: List[RSM_Command], ld: Int) extends KompicsEvent;
 
-  case class Accept(nL: Long, c: RSM_Command) extends KompicsEvent;
+  case class Accept(nL: (Int, Int), c: RSM_Command) extends KompicsEvent;
 
-  case class Accepted(nL: Long, m: Int) extends KompicsEvent;
+  case class Accepted(nL: (Int, Int), m: Int) extends KompicsEvent;
 
-  case class Decide(ld: Int, nL: Long) extends KompicsEvent;
+  case class Decide(ld: Int, nL: (Int, Int)) extends KompicsEvent;
 
   object State extends Enumeration {
     type State = Value;
@@ -59,35 +60,54 @@ class LeaderBasedSequencePaxos(init: Init[LeaderBasedSequencePaxos]) extends Com
     import Role._
     import State._
 
-    val sc = provides[SequenceConsensus];
+    // reconfig
+    var sigma = List.empty[RSM_Command];
+
+
+  val sc = provides[SequenceConsensus];
     val ble = requires[BallotLeaderElection];
     //val pl: Nothing = requires(FIFOPerfectLink)
     val net = requires[Network]
 
-    val (self, pi, others) = init match {
-      case Init(addr: NetAddress, pi: Set[NetAddress] @unchecked) => (addr, pi, pi - addr)
+    val (self, pi, others, c, rself, ri, rothers) = init match {
+      case Init(addr: NetAddress,
+      pi: Set[NetAddress] @unchecked, // set of processes in config c
+      c: Int, // configuration c
+      rself: (NetAddress, Int), // Repnumber of this one
+      ri:mutable.Map[NetAddress, Int], // set of replicas in config c (ip:port, Repnumber)
+      rothers:mutable.Map[NetAddress, Int]) // set of replicas in config c without this one
+      => (addr, pi, pi - addr, c, rself, ri, ri-addr)//c = configuration i, ri: RID = Netaddr of process, id
     }
     val majority = (pi.size / 2) + 1;
 
     var state = (FOLLOWER, UNKNOWN);
-    var nL = 0l;
-    var nProm = 0l;
-    var leader: Option[NetAddress] = None;
-    var na = 0l;
-    var va = List.empty[RSM_Command];
-    var ld = 0;
-    // reconfiguration
-    var cL = 0; //configuration i
-    var ca = 0; //configuration i
+  var leader: Option[NetAddress] = None;
 
-    // leader state
-    var propCmds = List.empty[RSM_Command];
-    val las = mutable.Map.empty[NetAddress, Int];
-    val lds = mutable.Map.empty[NetAddress, Int];
-    var lc = 0;
-    val acks = mutable.Map.empty[NetAddress, (Long, List[RSM_Command])];
+  // proposer state
+    var nL= (c,0);
+  val promises = mutable.Map.empty[Int, ((Int, Int), List[RSM_Command])];
+  val las = mutable.Map.empty[NetAddress, Int];
+  val lds = mutable.Map.empty[(NetAddress, Int), Int];
+  for (p <- pi){
+    las += (p -> sigma.size)
+  }
+  for (r <- ri){
+    lds += (r -> 0);
+  }
+  var propCmds = List.empty[RSM_Command];
+  var lc = sigma.size;
 
-    def suffix(s: List[RSM_Command], l: Int): List[RSM_Command] = {
+  // acceptor state
+  var nProm = (c,0);
+  var na = (c,0);
+  var va = sigma;
+
+  // learner state
+  var ld = sigma.size
+  // todo: How to compare the SSi and what is SSi??
+  var SSi = OperationToPropose(_, Op("STOP", "","",""))
+
+  def suffix(s: List[RSM_Command], l: Int): List[RSM_Command] = {
       s.drop(l)
     }
 
@@ -95,72 +115,102 @@ class LeaderBasedSequencePaxos(init: Init[LeaderBasedSequencePaxos]) extends Com
       s.take(l)
     }
 
-    ble uponEvent {
-      case BLE_Leader(l, n) => {
+  // define SSI("Stop", set of ps, ci, set of pids)
+
+  // fun stopped
+  def stopped(): Boolean = {
+    return (va.last.equals(SSi)); //??
+  }
+
+    ble uponEvent { // updated for reconfig
+      case BLE_Leader(l, b) => {
+        var n = (c , b)
         log.info(s"Proposing leader: $l [$self] (n: $n, nL: $nL)\n")
-        if (n > nL){
+        /*if (n > nL){
           leader = Some(l);
-          nL = n;
-          if (self == l && nL > nProm){
+          nL = n;*/
+          if (self == l && (n._1 == nL._1 && n._2 > nL._2)){ // what to compare?
             log.info(s"The leader is host: [$self]\n")
+            (nL, nProm) = (n,n)
             state = (LEADER, PREPARE);
-            propCmds = List.empty[RSM_Command];
-            las.clear();
-            lds.clear();
-            acks.clear();
-            lc = 0;
-            for (p <- others){
-              trigger(NetMessage(self, p, Prepare(nL, ld, na)) -> net);
+            promises += (rself._2 -> (na, suffix(va, ld)))// data structure
+            //propCmds = List.empty[RSM_Command];
+            for (p <- pi){
+              las += (p -> sigma.size);
             }
-            acks(l) = (na, suffix(va, ld));
-            lds(self) = ld;
-            nProm = nL;
+            lds.clear();
+            lds(rself) = ld;
+            //promises.clear();
+            lc = sigma.size;
+            for (r <- rothers){
+              trigger(NetMessage(self, r._1, Prepare(nL, ld, na)) -> net);
+            }
+            //promises(l) = (na, suffix(va, ld));
+            //lds(self) = ld;
+            //nProm = nL;
           } else{
             state = (FOLLOWER, state._2);
           }
         }
       }
-    }
 
-    net uponEvent {
+  // upon connection lost
+  // upon preparereq
+
+  net uponEvent {
       case NetMessage(p, Prepare(np, ldp, n)) => {
-        if (nProm < np){
+        if (nProm._1 == np._1 && nProm._2 > np._2){
           nProm = np;
           state = (FOLLOWER, PREPARE);
           var sfx = List.empty[RSM_Command];
-          if (na >= n){
+          if (na._1 == n._1 && na._2 >= n._2 ){
             sfx = suffix(va,ld);
           }
           trigger(NetMessage(self, p.src, Promise(np, na, sfx, ld)) -> net);
         }
       }
-      case NetMessage(a, Promise(n, na, sfxa, lda)) => {
+      case NetMessage(a, Promise(n, na, sfxa, lda)) => { // update reconfig
         if ((n == nL) && (state == (LEADER, PREPARE))) {
           log.info(s"Promise issued with leader: ${a.src}")
 
-          acks(a.src) = (na, sfxa);
-          lds(a.src) = lda;
-          val P: Set[NetAddress] = pi.filter(x => acks.get(x) != None);
+          promises(ri(a.src)) = (na, sfxa);
+          lds((a.src, ri(a.src))) = lda;
+          val P: Set[NetAddress] = pi.filter(x =>  promises(ri(x)) != None);
           if (P.size == (pi.size+1)/2) {
-            var ack = P.iterator.reduceLeft((v1,v2) => if (acks(v1)._2.size > acks(v2)._2.size) v1 else v2);
-            var (k, sfx) = acks(ack);
-            va = prefix(va, ld) ++ sfx ++ propCmds;
+            var ack = P.iterator.reduceLeft((v1, v2) => if (promises(ri(v1))._2.size > promises(ri(v2))._2.size) v1 else v2);
+            var (k, sfx) = promises(ri(ack));
+            //va = prefix(va, ld) ++ sfx ++ propCmds;
+            va = prefix(va, ld) ++ sfx
+
+            if (SSi == va.last) {
+              propCmds = List.empty;
+            } else if (propCmds.contains(SSi)) { // odering SSi as the last one to add to va
+                for (c <- (propCmds - SSi)) {
+                  va += c
+                }
+                va += SSi
+              } else {
+              for (c <- propCmds) {
+                va += c
+              }
+            }
             las(self) = va.size;
-            propCmds = List.empty;
+            //propCmds = List.empty;
             state = (LEADER, ACCEPT);
-            for (p <- others.filter(x => lds.get(x) != None)){
-              var sfxp = suffix(va,lds(p));
-              trigger(NetMessage(self, p, AcceptSync(nL, sfxp, lds(p))) -> net);
+
+            for (r <- rothers.filter(x => lds(x) != None && lds(x) != va.size)){
+              var sfxp = suffix(va,lds(r));
+              trigger(NetMessage(self, r._1, AcceptSync(nL, sfxp, lds(r))) -> net);
             }
           }
         } else if ((n == nL) && (state == (LEADER, ACCEPT))) {
           log.info(s"Late request for Promise from: ${a.src}")
 
-          lds(a.src) = lda;
-          var sfx = suffix(va,lds(a.src));
-          trigger(NetMessage(self, a.src, AcceptSync(nL, sfx, lds(a.src))) -> net);
-          if (lc != 0){
-            trigger(NetMessage(self, a.src, Decide(ld, nL)) -> net);
+          lds((a.src, ri(a.src))) = lda;
+          var sfx = suffix(va,lds((a.src, ri(a.src))));
+          trigger(NetMessage(self, a.src, AcceptSync(nL, sfx, lds((a.src, ri(a.src))))) -> net);
+          if (lc != sigma.size){ //??
+            trigger(NetMessage(self, a.src, Decide(lc, nL)) -> net);
           }
         }
       }
@@ -186,31 +236,32 @@ class LeaderBasedSequencePaxos(init: Init[LeaderBasedSequencePaxos]) extends Com
           }
         }
       }
-      case NetMessage(a, Accepted(n, m)) => {
+      case NetMessage(a, Accepted(n, la)) => { //changed for reconf
         if ((n == nL) && (state == (LEADER, ACCEPT))) {
-          las(a.src) = m;
-          var x =  pi.filter(x => las.getOrElse(x, 0) >= m);
-          if (lc < m && x.size >= (pi.size+1)/2){
-            lc = m;
-            for (p <- pi.filter(x => lds.get(x) != None)){
+          las(a.src) = la;
+          var x =  pi.filter(x => las.getOrElse(x, 0) >= la);
+          if (lc < la && x.size >= (pi.size+1)/2){
+            lc = la;
+            for (p <- pi.filter(x => lds(x, c) != None)){ //?????????
               trigger(NetMessage(self, p, Decide(lc, nL)) -> net);
             }
           }
         }
       }
 
-        sc uponEvent {
+        sc uponEvent { // updated for reconfig
           case SC_Propose(c) => {
             log.info(s"The command {} was proposed!", c)
             log.info(s"The current state of the node is {}", state)
             if (state == (LEADER, PREPARE)) {
               propCmds = propCmds ++ List(c);
             }
-            else if (state == (LEADER, ACCEPT)) {
+            else if (state == (LEADER, ACCEPT) && !stopped()) {
               va = va ++ List(c);
-              las(self) = las(self) + 1;
-              for (p <- others.filter(x =>  lds.get(x) != None)){
-                trigger(NetMessage(self, p, Accept(nL, c)) -> net);
+              //las(self) = las(self) + 1;
+              las(self) = va.size
+              for (r <- rothers.filter(x =>  lds(x) != None)){
+                trigger(NetMessage(self, r._1, Accept(nL, c)) -> net);
               }
             }
           }
